934ca44e622d5ff375608602fc9241113918b5a7
EC2 Default User <ec2-user@ip-172-31-4-148.ec2.internal>
2018-11-12 13:07:43 +0000

    難しい

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :correct_user, only: [:edit, :destroy]
+  before_action :require_user_logged_in
+  
   def index
     @tasks = Task.all
   end
